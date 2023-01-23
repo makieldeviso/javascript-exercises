c1cec8d58f7f3536edf8208edd62361256e268d2
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-01-23 23:47:07 +0800

    Adds code to run javascript exercise and pass tests
    Solves the problem using a math formula.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+
+// using Arithmetic Progression Formula
+
+if (firstNum < 0 || lastNum < 0) {
+    return "ERROR";
+} else if (typeof firstNum !== "number" || typeof lastNum !== "number") {
+    return "ERROR";
+}
+
+let numOfDigits = (firstNum + lastNum - 1);
+console.log(numOfDigits)
+
+let total = (numOfDigits / 2) * (firstNum + lastNum);
+
+return total;
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
