91d70c919f1348704b0fb590e56e1b5ff6c0c5fd
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-01-23 22:18:51 +0800

    Adds code to run javascript exercise and pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(inputString, repeats) {
+    
+    let newString = "";
 
+    if (repeats < 0) {
+        return "ERROR"
+    }
+
+    for (let i = 0; i < repeats; i++) {
+        newString += inputString;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
