c0ede0f45a48bd3a79f8f8e56b76f27e38c51bb5
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-01-23 22:44:28 +0800

    Adds code to run javascript exercise and pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    let newString = ""
+    for (let i = (inputString.length - 1); i >= 0; i--) {
+        newString += inputString[i];
+
+    }
+
+    return newString;
+
 
 };
 
