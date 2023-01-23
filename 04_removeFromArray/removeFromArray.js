90102370349aeb39e8bc2c3a9741137c10751a61
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-01-23 23:20:22 +0800

    Adds code to run javascript exercise and pass tests
    Removes other arguments from first argument Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,33 @@
 const removeFromArray = function() {
 
+// put the arguments into args Array
+    let args = [...arguments];
+    
+
+// take the first argument as base Array
+    let baseArr = args[0];
+    
+
+// takes the rest of the arguments to another Array
+    let restArr = [];
+    for (let i = 1; i < args.length; i++ ) {
+        restArr.push(args[i]);
+    }
+    
+// counter check baseArr with restArr
+
+    let finalArr = [];
+
+    for (let i = 0; i < baseArr.length; i++) {
+        if (restArr.includes(baseArr[i]) === false) {
+         finalArr.push(baseArr[i]);
+    }
+}
+
+return finalArr;
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
