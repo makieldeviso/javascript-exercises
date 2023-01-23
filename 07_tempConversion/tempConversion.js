f6410dd90f47960e3e16713a6e0e698888ef9c24
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-01-24 00:59:14 +0800

    Adds code to run javascript exercise and pass tests
    Converts temperature in Fahrenheit to Celsius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+
+let celsiusTemp = (fahrenheitTemp - 32) * (5 / 9);
+
+if (celsiusTemp !== Math.trunc(celsiusTemp)) {
+  return Number(celsiusTemp.toFixed(1));
+} else {
+  return celsiusTemp;
+}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+
+  let fahrenheitTemp = celsiusTemp * (9 / 5 ) + 32;
+
+  if (fahrenheitTemp !== Math.trunc(fahrenheitTemp)) {
+    return Number(fahrenheitTemp.toFixed(1));
+  } else {
+    return fahrenheitTemp;
+  }
+
 };
+
 
 // Do not edit below this line
 module.exports = {
