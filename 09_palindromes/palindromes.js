4601683345ea327e1f248b56dac686add0df4c88
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-02-14 01:57:41 +0800

    Adds code to run javascript exercise and pass tests
    Detects an input string if it is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,34 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let letterRegex = /[a-zA-Z]/;
 
+    let filteredLetters = [];
+    let phraseBreakDown = phrase.split("");
+
+    phraseBreakDown.forEach(char => {
+      if (letterRegex.test(char)) {
+        filteredLetters.push(char.toLowerCase())
+      };
+    });
+
+    let reversed = [];
+    filteredLetters.forEach(char => {
+      reversed.unshift(char);
+    })
+
+  console.log(filteredLetters);
+  console.log(reversed);
+
+  let result = true;
+    for (let i = 0; i < filteredLetters.length; i++) {
+      if (filteredLetters[i] != reversed[i]) {
+        result = false;
+        break;
+      }
+        result = true;
+    }
+
+  return result;
+  
 };
 
 // Do not edit below this line
