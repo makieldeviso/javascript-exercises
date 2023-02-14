59b15f422c0cc48309ba41e69466a4f966512785
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-02-14 15:30:09 +0800

    Adds code to run javascript exercise and pass tests
    Takes Array and return array of titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(library) {
 
+    let titlesArray = [];
+    library.forEach(book => {
+        titlesArray.push(book["title"]);
+    })
+
+    return titlesArray;
 };
 
 // Do not edit below this line
