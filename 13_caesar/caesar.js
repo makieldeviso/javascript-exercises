3a883d02a70f6a6a9dec407feb9eadb77a81db00
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-02-14 20:37:57 +0800

    Adds code to run javascript exercise and pass tests
    Solves Caesar cipher

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,39 @@
-const caesar = function() {
+const caesar = function(phrase, shift) {
+    
+    let phraseArr = phrase.split("");
+    console.log(phraseArr);
 
+    let phraseCodeArr = [];
+    phraseArr.forEach(char => {
+        let charCode = char.charCodeAt() + shift;
+        
+        if (/[A-Z]/.test(char)) {
+                if (charCode > 90 && charCode < 127) {
+                    phraseCodeArr.push(String.fromCharCode(charCode - 26));
+                } else if(charCode > 127) {
+                     phraseCodeArr.push(String.fromCharCode(charCode - (Math.floor((charCode -65)/26)* 26)));
+                    
+                }else if (charCode < 65) {
+                    phraseCodeArr.push(String.fromCharCode(charCode + 26));
+                    
+                } else {
+                    phraseCodeArr.push(String.fromCharCode(charCode));
+                }
+        } else if (/[a-z]/.test(char)) {
+                if (charCode > 122 && charCode < 127) {
+                    phraseCodeArr.push(String.fromCharCode(charCode - 26));
+                } else if (charCode > 127) {
+                    phraseCodeArr.push(String.fromCharCode(charCode - (Math.floor((charCode -97)/26)* 26)));
+                } else if (charCode < 97) {
+                    phraseCodeArr.push(String.fromCharCode(charCode + 26));
+                } else {
+                    phraseCodeArr.push(String.fromCharCode(charCode));
+                }
+        } else {
+            phraseCodeArr.push(char);
+        }
+    })
+return phraseCodeArr.join("");
 };
 
 // Do not edit below this line
