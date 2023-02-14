6c9c8dbd386d7cea0774beae9e91e9464498473c
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-02-14 13:24:38 +0800

    Adds code to run javascript exercise and pass tests
    Return the nth number(input) of the fibonacci the sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (typeof number === "string") {
+     return fibonacci(parseInt(number));    
+    }
+  
+    if (number <= 0) {
+        return "OOPS";
+    }
 
+    let sequence = [1];
+    
+    for (let i = 0; i < number; i++) {
+        if (sequence.length === number) {
+            break;
+        } else if (sequence.length === 1) {
+            sequence.push(sequence[0]);
+        } 
+        
+        if (sequence.length === number) {
+            break;
+        }
+        
+        sequence.push(sequence[i + 1] + sequence[i]);
+    }
+    return sequence[sequence.length - 1];
 };
 
 // Do not edit below this line
