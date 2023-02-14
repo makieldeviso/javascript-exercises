a8f93759f1ef8f7993436c4b3730c62c12b3b6e1
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-02-14 16:36:55 +0800

    Adds code to run javascript exercise and pass tests
    Finds the oldest in an array of people

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let sortedPeople =  people.sort((person, nextPerson) => {
+
+        let personAge;
+        let nextPersonAge;
+
+        if (person["yearOfDeath"] === undefined) {
+            personAge = new Date().getFullYear() - person["yearOfBirth"];  
+        } else {
+            personAge = person["yearOfDeath"] - person["yearOfBirth"];
+        }
+        
+        if (nextPerson["yearOfDeath"] === undefined) {
+            nextPersonAge = new Date().getFullYear() - nextPerson["yearOfBirth"];
+        } else {
+            nextPersonAge = nextPerson["yearOfDeath"] - nextPerson["yearOfBirth"];
+        }
+        
+        return personAge < nextPersonAge ? -1 : 1;
+        
+    });
+    console.table(sortedPeople);
+    return sortedPeople[sortedPeople.length - 1];
 
 };
 
